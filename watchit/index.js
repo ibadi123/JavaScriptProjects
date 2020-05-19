25eeb3b2b2c7448b8878d8738c979ae50f613abc
ibadi123 <ibadi12345@gmail.com>
2020-05-19 23:03:59 +0500

    Added on 17-5-20

---
+++ watchit/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
+const chalk = require("chalk");
+
+program
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error("Can't find the file -> " + name);
+    }
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.blueBright.bgBlackBright(">>> Starting Process..."));
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 1000);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
---
+++ watchit/test.js
@@ -0,0 +1,5 @@
+const myName = "Syed Ibad";
+
+console.log(`My name is  ${myName}`);
+
+console.log('Edit #2')
